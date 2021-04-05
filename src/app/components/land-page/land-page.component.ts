b5ab2a1ad9646a5868c81dd2d7859ef96c96fcfc
Mondrian92 <m.maugeri92@gmail.com>
2021-04-05 23:18:35 +0200

    land page first version

--- src/app/components/land-page/land-page.component.ts
+++ src/app/components/land-page/land-page.component.ts
@@ -8,68 +8,80 @@ import { City, ForecastRes, mappedForecast } from 'src/app/interfaces/forecast';
   styleUrls: ['./land-page.component.scss'],
 })
 export class LandPageComponent implements OnInit {
+  constructor(private apiCaller: ForecastService) {}
 
-  constructor(private apiCaller:ForecastService) { }
-
-  ngOnInit():void {
-    this.getLocation()
+  ngOnInit(): void {
+    this.getLocation();
+    
   }
 
-  longitude: number
-  latitude: number
-  forecast: ForecastRes | undefined 
-  dayKey: string = ""
+  tab: string;
+  longitude: number;
+  latitude: number;
+  forecast: ForecastRes | undefined;
+  dayKey: string = '';
   displayedColumns: string[] = [
-    "date",
-    "average",
-    "minimum", 
-    "maximum", 
-    "pressure",
-    "humidity",
-    "wind",
-    "condition",
-    "icon"
-  ]
+    'date',
+    'average',
+    'minimum',
+    'maximum',
+    'pressure',
+    'humidity',
+    'wind',
+    'condition',
+    'icon',
+  ];
 
   getLocation(): void {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(async (position) => {
-          this.longitude = position.coords.longitude;
-          this.latitude = position.coords.latitude;
-          this.forecast = await this.apiCaller.foreCoordAll(this.longitude, this.latitude, "", "", "", "", "metric")
-        });
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        this.longitude = position.coords.longitude;
+        this.latitude = position.coords.latitude;
+        this.forecast = await this.apiCaller.foreCoordAll(
+          this.longitude,
+          this.latitude,
+          '',
+          '',
+          '',
+          '',
+          'metric'
+        );
+      });
     } else {
-       console.log("No support for geolocation")
+      console.log('No support for geolocation');
     }
   }
-  setDayKey(dayKey){
-    this.dayKey = dayKey
-    console.log(this.mappedForecast['2021-04-03'])
+
+  changeTab = (tabValue) => this.tab = tabValue;
+
+  setDayKey(dayKey) {
+    this.dayKey = dayKey;
+    console.log(this.mappedForecast['2021-04-03']);
   }
-  test(item){
-    console.log(item)
+
+  test(item) {
+    console.log(item);
   }
 
   get mappedForecast(): mappedForecast {
     return this.forecast?.forecast.reduce((acc, value) => {
-      const date = value.time.split(" ", 1)[0];
+      const date = value.time.split(' ', 1)[0];
       return {
         ...acc,
-        [date]: [...(acc[date] ? acc[date] : []), value]
+        [date]: [...(acc[date] ? acc[date] : []), value],
       };
-    }, {})
+    }, {});
   }
+  
   get grouppedForecast(): string[] {
-    return this.mappedForecast ? Object.keys(this.mappedForecast) : []
-}
- 
+    return this.mappedForecast ? Object.keys(this.mappedForecast) : [];
+  }
 
   get city(): City {
-    return this.forecast?.city
+    return this.forecast?.city;
   }
 
   delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
-}
-
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 }
--- src/app/components/land-page/land-page.module.ts
+++ src/app/components/land-page/land-page.module.ts
@@ -4,13 +4,15 @@ import { RoutingLandModule } from './routing-land.module';
 import { LandPageComponent } from './land-page.component';
 import { RouterLoginModule } from '../login/router-login.module';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
 
 
 
 @NgModule({
   declarations: [LandPageComponent],
   imports: [
-    CommonModule,RoutingLandModule,RouterLoginModule,FormsModule,
+    CommonModule, RoutingLandModule, RouterLoginModule, FormsModule, IonicModule
   ]
 })
 export class LandPageModule { }
